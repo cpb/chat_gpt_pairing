dfcb8d82d69e18f6bd0413400dabfdacb4a886ef
Caleb Buxton <me@cpb.ca>
2022-12-04 13:00:30 -0800

    fail: Adds proposed implementation for Tree and convert_to_vector
    Prompt: F
    
    Failures:
    
      1) convert_to_vector converts an n-ary tree into a vector
         Failure/Error: let(:tree) { Tree.new(1, [Tree.new(2, [Tree.new(4)]), Tree.new(3)]) }
    
         NameError:
           uninitialized constant Tree
         # ./convert_to_vector_spec.rb:2:in `block (2 levels) in <top (required)>'
         # ./convert_to_vector_spec.rb:6:in `block (2 levels) in <top (required)>'
    
    Finished in 0.00716 seconds (files took 0.14657 seconds to load)
    1 example, 1 failure
    
    Failed examples:
    
    rspec ./convert_to_vector_spec.rb:5 # convert_to_vector converts an n-ary tree into a vector
    
    Now we are in implementation mode. Write an implementation that passes this rspec test.

--- tree_to_vector_to_tree/convert_to_vector_spec.rb
+++ tree_to_vector_to_tree/convert_to_vector_spec.rb
@@ -1,3 +1,24 @@
+class Tree
+  attr_reader :value, :children
+
+  def initialize(value, children = [])
+    @value = value
+    @children = children
+  end
+end
+
+def convert_to_vector(tree)
+  vector = []
+
+  def convert_to_vector_helper(tree, vector)
+    vector << tree.value
+    tree.children.each { |child| convert_to_vector_helper(child, vector) }
+  end
+
+  convert_to_vector_helper(tree, vector)
+  vector
+end
+
 describe 'convert_to_vector' do
   let(:tree) { Tree.new(1, [Tree.new(2, [Tree.new(4)]), Tree.new(3)]) }
   let(:vector) { [1, 2, 3, 4] }
