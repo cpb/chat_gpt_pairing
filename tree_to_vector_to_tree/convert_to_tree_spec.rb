c1787bbee0cff0cd51c06864549b84079072015a
Caleb Buxton <me@cpb.ca>
2022-12-04 13:11:09 -0800

    fail: first attempt to implement convert_to_tree to pass the spec
    Prompt: Very good. Now we are in implementation mode. Here is the rspec failure I got:
    
    F
    
    Failures:
    
      1) convert_to_tree converts a vector into an n-ary tree
         Failure/Error: expect(convert_to_tree(vector)).to eq(tree)
    
         NoMethodError:
           undefined method `convert_to_tree' for #<RSpec::ExampleGroups::ConvertToTree "converts a vector into an n-ary tree" (./convert_to_tree_spec.rb:5)>
         # ./convert_to_tree_spec.rb:6:in `block (2 levels) in <top (required)>'
    
    Finished in 0.00329 seconds (files took 0.09672 seconds to load)
    1 example, 1 failure
    
    Failed examples:
    
    rspec ./convert_to_tree_spec.rb:5 # convert_to_tree converts a vector into an n-ary tree

--- tree_to_vector_to_tree/convert_to_tree_spec.rb
+++ tree_to_vector_to_tree/convert_to_tree_spec.rb
@@ -1,3 +1,31 @@
+class Tree
+  attr_reader :value, :children
+
+  def initialize(value, children = [])
+    @value = value
+    @children = children
+  end
+end
+
+def convert_to_tree(vector)
+  tree = nil
+  queue = []
+
+  vector.each do |value|
+    current = Tree.new(value)
+
+    if queue.empty?
+      tree = current
+    else
+      queue.last.children << current
+    end
+
+    queue << current
+  end
+
+  tree
+end
+
 describe 'convert_to_tree' do
   let(:tree) { Tree.new(1, [Tree.new(2, [Tree.new(4)]), Tree.new(3)]) }
   let(:vector) { [1, 2, 3, 4] }
