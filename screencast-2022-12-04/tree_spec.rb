892aed2539697615b4246d23f5dc27e151c0b12b
Caleb Buxton <me@cpb.ca>
2022-12-04 15:19:00 -0800

    Initial implementation passing the test

--- screencast-2022-12-04/tree_spec.rb
+++ screencast-2022-12-04/tree_spec.rb
@@ -1,3 +1,59 @@
+class Tree
+  def initialize
+    @root = nil
+  end
+
+  def insert(value)
+    if @root.nil?
+      @root = TreeNode.new(value)
+    else
+      @root.insert(value)
+    end
+  end
+
+  def to_vector
+    vector = []
+    traverse(@root, vector)
+    vector
+  end
+
+  private
+
+  def traverse(node, vector)
+    return if node.nil?
+    vector << node.value
+    traverse(node.left, vector)
+    traverse(node.right, vector)
+  end
+end
+
+class TreeNode
+  attr_reader :value
+  attr_accessor :left, :right
+
+  def initialize(value)
+    @value = value
+    @left = nil
+    @right = nil
+  end
+
+  def insert(value)
+    if value <= @value
+      if @left.nil?
+        @left = TreeNode.new(value)
+      else
+        @left.insert(value)
+      end
+    else
+      if @right.nil?
+        @right = TreeNode.new(value)
+      else
+        @right.insert(value)
+      end
+    end
+  end
+end
+
 RSpec.describe Tree do
   describe '#to_vector' do
     it 'should convert an n-ary tree into a vector' do
